a4398cb1d9a32c35866069221e74275a6b309d90
sug1chan <sug1chan@ymail.ne.jp>
2025-09-29 00:24:50 +0900

    add code

--- utils/camera.py
+++ utils/camera.py
@@ -1,3 +1,29 @@
 class Camera():
-    def __init__(self):
-        pass
+    run = False
+
+    def __init__(self,
+                 dev_id = 0, ):
+        self.cap = cv2.VideoCapture(dev_id)
+        self.run = True
+
+    def read(self):
+        if self.run and self.isOpened():
+            return self.cap.read()
+        else:
+            # TODO: Error Handling
+            self.run = False
+
+    def isOpened(self, ):
+        if !self.run:
+            # TODO: Error Handling
+            pass
+        return self.cap.isOpened()
+
+    def release(self, ):
+        if self.run:
+            self.cap.release()
+            self.run = False
+        else:
+            # TODO: Error Handling
+            pass
+

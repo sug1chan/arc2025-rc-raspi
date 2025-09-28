f424f8c48f51746feb6a2ca52544e2849295348c
sug1chan <sug1chan@ymail.ne.jp>
2025-09-29 00:20:28 +0900

    add code

--- utils/socket.py
+++ utils/socket.py
@@ -1,8 +1,35 @@
-class socket, time
+import socket, time
 from threading import Thread, Event
 
+SOCK_TIMEOUT = 3.
+
 class Socket():
-    def __init__(self, addr, port):
-        self.addr = addr
-        self.port = port
+    sfd = None
+
+    def __init__(self,
+                 addr: str,
+                 port: int,
+                 sock_timeout: int = SOCK_TIMEOUT):
+        self.addr         = addr
+        self.port         = port
+        self.sock_timeout = sock_timeout
+
+    def connect(self, ):
+        self.sfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sfd.settimeout(self.sock_timeout)
+        self.sfd.connect()
+        pass
+
+    def send(self,
+             msg):  # TODO: 型が何なのか調べる。
+        if self.sfd is None:
+            # TODO: Error Handling
+            pass
+        pass
+
+    def close(self, ):
+        if self.sfd is None:
+            # TODO: Error Handling
+            pass
+        pass
 

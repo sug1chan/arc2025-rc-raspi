3891e363516a35199c0637103ec4c8b6dd0c7e14
sug1chan <sug1chan@ymail.ne.jp>
2025-10-05 23:34:34 +0900

    add test function

--- utils/yolo.py
+++ utils/yolo.py
@@ -1,10 +1,24 @@
 from camera import Camera
+from ultralytics import YOLO
+
 class DetectEGP():
-    def __init__(self, model):
-        self.model = model
+    def __init__(self,
+                 weights):
+        self.weights = weights
+
+        # setup
+        self.yolo    = YOLO(self.weights)
+
+    def detect(self,
+               frame):
+        return self.yolo(frame)
+
+    def track(self, frame):
+        pass
 
 def test():
-    MODEL = "../data/first_model"
+    MODEL = "../data/first_model.pt"
+    VIDEO = "../data/"
 
     model = DetectEGP(model = MODEL)
     cam = Camera()
@@ -14,6 +28,7 @@ def test():
 
         try:
             frame = cam.read()
+            frame = model.detect(frame)
             cv2.imshow('test camera', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):

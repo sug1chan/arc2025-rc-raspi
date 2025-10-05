d3449b68c6df255422c9f83eabd38dd688630222
sug1chan <sug1chan@ymail.ne.jp>
2025-10-05 14:26:47 +0900

    add test code

--- utils/camera.py
+++ utils/camera.py
@@ -1,3 +1,5 @@
+import cv2
+
 class Camera():
     run = False
 
@@ -6,15 +8,21 @@ class Camera():
         self.cap = cv2.VideoCapture(dev_id)
         self.run = True
 
-    def read(self):
+    def read(self, ):
         if self.run and self.isOpened():
-            return self.cap.read()
+            ret, frame = self.cap.read()
+
+            if ret:
+                return frame
+            else:
+                # TODO: Error Handling
+                self.run = False
         else:
             # TODO: Error Handling
             self.run = False
 
     def isOpened(self, ):
-        if !self.run:
+        if not self.run:
             # TODO: Error Handling
             pass
         return self.cap.isOpened()
@@ -26,4 +34,22 @@ class Camera():
         else:
             # TODO: Error Handling
             pass
+
+def test():
+    cam = Camera()
+
+    while cam.isOpened():
+        try:
+            frame = cam.read()
+            cv2.imshow('test camera', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except:
+            break
+        
+    cam.release()
+
+if __name__ == "__main__":
+    test()
 

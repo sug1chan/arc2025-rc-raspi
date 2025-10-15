ddf6af946698b3fa6125331a6cb286bffb001d78
sug1chan <sug1chan@ymail.ne.jp>
2025-10-15 22:34:33 +0900

    add BBoxes class

--- utils/yolo.py
+++ utils/yolo.py
@@ -4,6 +4,32 @@ from ultralytics import YOLO
 
 DEFAULT_THRESHOLD = .9
 
+class BBoxes():
+    def __init__(self,
+                 result, ):
+
+        self.result = result[0] if not result is None else None
+
+        if not self.result is None:
+            self.img    = self.result.orig_img
+            self.label  = self.result.names
+            self.shape  = self.result.orig_shape
+    
+    def get_bboxes(self, ):
+        return self.result.boxes.xyxy
+
+    def plot(self,
+             *args,
+             **kwargs, ):
+        return self.result.plot(*args,
+                                **kwargs)
+
+    def isNone(self, ):
+        return self.result is None
+
+    def __iter__(self, ):
+        yield from self.get_bboxes()
+
 class DetectEGP():
     def __init__(self,
                  weights,
@@ -18,16 +44,16 @@ class DetectEGP():
 
     def detect(self,
                frame,
-               save = False,):
-        return self.yolo(frame,
-                         conf    = self.threshold,
-                         verbose = self.verbose,
-                         save    = save)
+               save = False, ):
+        return BBoxes(self.yolo(frame,
+                                conf    = self.threshold,
+                                verbose = self.verbose,
+                                save    = save))
 
     def track(self, frame):
-        return self.yolo.track(frame,
-                               conf    = self.threshold, 
-                               verbose = self.verbose, )
+        return BBoxes(self.yolo.track(frame,
+                                      conf    = self.threshold, 
+                                      verbose = self.verbose, ))
 
 def test():
     MODEL  = "../data/win/data/first_model.pt"
@@ -46,19 +72,19 @@ def test():
     while cam.isOpened():
         try:
             frame = cam.read()
-            #res = model.detect(frame, save = False)
-            res = model.track(frame)
+            res = model.detect(frame, save = False)
+            # res = model.track(frame)
 
-            if res is None:
+            if res.isNone():
                 break
 
-            annotated_frame = res[0].plot()
+            annotated_frame = res.plot()
 
             # cv2.imshow('test camera', annotated_frame)
             out.write(annotated_frame)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
 
         except Exception as e:
             print(e)

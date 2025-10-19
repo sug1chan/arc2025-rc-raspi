22b448db9d448e9d57c04045700f17def6e3e93e
sug1chan <sug1chan@ymail.ne.jp>
2025-10-19 09:26:57 +0900

    add src

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+# --- OS settings -------------------------------
+
+IP_ADDR  = "192.168.250.2"
+TCP_PORT = 8000
 
 # --- yolo settings -----------------------------
 
 YOLO_MODEL_PATH = "./data/model"
+YOLO_IS_SAVE    = False
+
+YOLO_THRESHOLD  = .9
 
--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,11 @@
-class Log():
-    def __init__(self):
-        pass
+import json
+from logging import getLogger, config
+
+def get_logger(name: str, json_path: str = None):
+    if not json_pth is None:
+        with open(json_path, 'r') as fd:
+            log_conf = json.load(f)
+            config.dictConfig(log_conf)
+   
+    return getLogger(name)
+

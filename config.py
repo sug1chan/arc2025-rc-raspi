3c3eca89025629adaf4cf8a1a55a244c49aedceb
sug1chan <sug1chan@ymail.ne.jp>
2025-10-26 15:04:18 +0900

    がんばった

--- config.py
+++ config.py
@@ -3,10 +3,25 @@
 IP_ADDR  = "192.168.250.2"
 TCP_PORT = 8000
 
+# --- camera settings ---------------------------
+
+FRONT_ID = 0
+BIRDS_EYE_ID = 0
+
 # --- yolo settings -----------------------------
 
 YOLO_MODEL_PATH = "./data/model"
-YOLO_IS_SAVE    = False
-
 YOLO_THRESHOLD  = .9
+
+YOLO_IS_SAVE    = False
+YOLO_IS_VERBOSE    = False
+
+
+# --- module settings ---------------------------
+
+# IR
+IR_PIN1 = 0
+IR_PIN2 = 1
+
+# GYRO
 
--- main.py
+++ main.py
@@ -6,32 +6,44 @@ from utils.ac_util import State, get_mode
 from utils.connection import *
 from utils.socket_utils import *
 
-import cv2
+import cv2, argparse
 
 DEFAULT_EPSILON = 64
+LOGCONF = "./logconf.json"
 
 class AutoControl():
     def __init__(self,
                  mode = 0,
-                 epsilon = DEFAULT_EPSILON):
-        self.model  = None
-        self.camera = 
-        self.sfd    = ClientSocketCom(cfg.IP_ADDR,
-                                      cfg.TCP_PORT)
-        self.gyro   = None
-        self.ir1    = None
-        self.ir2    = None
+                 epsilon = DEFAULT_EPSILON,
+                 logconf = LOGCONF):
+        # --- LOGGER SETTINGS ---
+        self.log    = get_logger(self.__class__.__name__,
+                                 logconf, )
+
+        # --- MODULE SETTINGS ---
+        self.model   = DetectEGP(cfg.YOLO_MODEL_PATH,
+                                 threshold = cfg.YOLO_THRESHOLD,
+                                 logger    = self.log,
+                                 verbose   = False)
+        self.fcamera = Camera(dev_id = cfg.FRONT_ID)
+        self.bcamera = Camera(dev_id = cfg.BIRDS_EYE_ID)
+
+        self.sfd     = ClientSocketCom(cfg.IP_ADDR,
+                                       cfg.TCP_PORT, )
+        self.gyro    = GyroSensor()
+        self.ir1     = IRSensor()
+        self.ir2     = IRSensor()
 
         # --- INIT MODE SETTINGS ---
         self.init_mode = get_mode(mode)
 
-        self.func = [_finish,
-                     _init,
-                     _detection,
-                     _approach,
-                     _adjustment,
-                     _cutting,
-                     _comeback, ]
+        self.func = [self._finish,
+                     self._init,
+                     self._detection,
+                     self._approach,
+                     self._adjustment,
+                     self._cutting,
+                     self._comeback, ]
 
     def init(self, ):
         self.mode   = State.INIT
@@ -107,9 +119,25 @@ class AutoControl():
         def _comeback(self, ):
             pass
 
+def get_parser():
+    parser = argparse.ArgumentParser(
+            prog = "ARC Robot AutoController",
+            add_help = True,
+            )
+
+    parser.add_argument("-m",
+                        "--mode",
+                        type = int,
+                        help = "動作モードを指定。デフォルト0。というか現状0しか使わん。",
+                        default = 0)
+
+    return parser.parse_args()
 
 def main():
-    pass
+    arg = get_parser()
+    print(arg.mode)
+    robot = AutoControl(mode = arg.mode,
+                        logconf = None, )
 
 if __name__ == "__main__":
     main()

e19701fd096d061c5fa3949fec5c48976096394e
MaSh-1009 <mash1009nico@gmail.com>
2025-11-01 12:02:23 +0900

    赤外線センサ_クラス作成
    赤外線センサ_クラス作成

--- utils/ir.py
+++ utils/ir.py
@@ -1,3 +1,11 @@
-class IRSensor():
-    def __init__(self):
-        pass
+from gpiozero import MotionSensor
+
+class IRSensor:
+    def __init__(self, pin):
+        self.sensor = MotionSensor(pin)
+
+    def is_detected(self):
+        return self.sensor.motion_detected  # True: 動きを検知中, False: 検知なし
+
+    def close(self):
+        self.sensor.close()
